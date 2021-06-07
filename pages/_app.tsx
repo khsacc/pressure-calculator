60eb6d4412d6cc1356736b7945379a9bc18d210d
khsacc <k.hiroki1.732@gmail.com>
2021-06-07 17:11:34 +0900

    added credit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Head from 'next/head';
-import { ThemeProvider } from '@material-ui/core/styles';
+import { makeStyles, ThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import {Theme} from '../styles/theme'
 
+const useStyle = makeStyles(() => ({
+  footer: {
+    paddingBottom: 20,
+    textAlign: 'center'
+  }
+}))
+
 export default function MyApp(props) {
   const { Component, pageProps } = props;
+  const classes = useStyle()
 
   React.useEffect(() => {
     // Remove the server-side injected CSS.
@@ -26,6 +34,10 @@ export default function MyApp(props) {
         {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
         <CssBaseline />
         <Component {...pageProps} />
+        <footer className={classes.footer}>
+          Hiroki Kobayashi, 2021<br />
+          Source code is available <a href="https://github.com/khsacc/pressure-calculator" rel="external" target="_blank">here</a>
+        </footer>
       </ThemeProvider>
     </React.Fragment>
   );
