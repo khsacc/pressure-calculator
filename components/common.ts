e33daeafee6ff65ac8c23697350831aa2b40957a
khsacc <hero.key629522@gmail.com>
2021-06-11 16:30:32 +0900

    made common styles summarized

--- components/common.ts
+++ components/common.ts
@@ -1,3 +1,5 @@
+import makeStyles from "@material-ui/core/styles/makeStyles";
+
 export const defaultValues = {
   integer: '693',
   decimal: '0',
@@ -7,3 +9,43 @@ export const calcR = (int: string, dec: string) => {
   const ret = Number(Number(int) + ((Number(dec) / (10 ** String(dec).length))));
   return ret
 };
+
+export const useCommonStyles = makeStyles((theme) => ({
+  container: {
+    minHeight: "100vh",
+    padding: "0 0.5rem",
+    display: "flex",
+    flexDirection: "column",
+    // justifyContent: 'center',
+    alignItems: "center",
+    height: "100vh",
+  },
+  heading: {
+    color: theme.palette.primary.main,
+  },
+  numericalInput: {
+    width: 100,
+  },
+  dot: {
+    fontWeight: "bold",
+    fontSize: "2em",
+    lineHeight: "2",
+    margin: "0 5px",
+  },
+  paper: {
+    padding: 10,
+    margin: '10px 0',
+  },
+  display: {
+    fontSize: 25,
+  },
+  section: {
+    paddingBottom: 10,
+    borderBottom: `1px solid ${theme.palette.primary.main}`,
+    borderTop: `1px solid ${theme.palette.primary.main}`
+  },
+  atMark: {
+    fontSize: '1.5em',
+    margin: "0 5px",
+  }
+}));
